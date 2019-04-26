8cb51d3ffefe738fd783e2cbcf36bf9627386642
Gyumeijie <18170879819@163.com>
2019-04-26 14:24:47 +0800

    add prefix to all paths on the site

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,10 @@ module.exports = {
     aboutTitle: "About: title from siteMetadata",
     contactTitle: "Contact: title from siteMetadata",
   },
+  // Many sites are hosted at something other than the root of their domain.
+  // So these sites need a prefix added to all paths on the site. For more 
+  // details, go to the https://www.gatsbyjs.org/docs/path-prefix/
+  pathPrefix: `/gatsby-site-demo`,
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
